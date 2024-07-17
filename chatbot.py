30cd4d8ccd1c8c67a0412768c1f88763f05a5c0f
Courtney Ross <134150039+courtneyleora@users.noreply.github.com>
2024-07-17 13:47:54 -0400

    new code

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,60 @@
 import random
+import json
+import re
+import requests
+
+
+
+
+def response(q):
+    language_code = 'en'
+    search_query = q
+    number_of_results = 1
+    headers = {
+        # 'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+        'User-Agent': 'YOUR_APP_NAME (YOUR_EMAIL_OR_CONTACT_PAGE)'
+    }
+
+    base_url = 'https://api.wikimedia.org/core/v1/wikipedia/'
+    endpoint = 'search/page'
+    url = f'{base_url}{language_code}/{endpoint}'
+    parameters = {'q': search_query, 'limit': number_of_results}
+    res = requests.get(url, headers=headers, params=parameters)
+    response = json.loads(res.text)
+
+    output = []
+    if response.get('pages', []):
+        for page in response['pages']:
+            display_title = page.get('title', 'No title')
+            article_url = f'https://{language_code}.wikipedia.org/wiki/{page.get("key", "")}'
+
+            try:
+                article_description = page['description']
+            except KeyError:
+                article_description = 'a Wikipedia article'
+
+            try:
+                thumbnail_url = 'https:' + page['thumbnail']['url']
+            except KeyError:
+                thumbnail_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/200px-Wikipedia-logo-v2.svg.png'
+
+            excerpt_text = page['excerpt']
+            excerpt_text_no_tags = re.sub('<span.*?>|&.*;|</span>', '', excerpt_text)
+            description_text = page.get('description', 'No description available')
+            output.append({'excerpt': excerpt_text_no_tags, 'description': description_text, 'article_url': article_url})
+    else:
+        output.append({'excerpt': '', 'description': '', 'article_url': ''})
+
+    return output
+
+def remove_punc(pstr):
+    punc = r'''!()-[]{};:'",<>./?@\#$%^&*_~'''
+    for ele in pstr:
+        if ele in punc:
+            pstr = pstr.replace(ele, "")
+    return pstr
+
+import random
 import requests
 import re
 import json
